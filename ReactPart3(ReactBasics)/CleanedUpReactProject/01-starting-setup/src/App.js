25e41bf1c7fd84a60744b119aa14f778173977ff
mrylraihan <mr.ylraihan@gmail.com>
2021-12-16 16:19:29 -0800

    react and css

--- ReactPart3(ReactBasics)/CleanedUpReactProject/01-starting-setup/src/App.js
+++ ReactPart3(ReactBasics)/CleanedUpReactProject/01-starting-setup/src/App.js
@@ -1,11 +1,12 @@
+import Car from "./components/Car";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>this is also visible!</p>
+      <h1>Let's get started!</h1>
       <ExpenseItem />
+      <Car carName={'Mustang'} cost={2000} type={'muscle'}/>
     </div>
   );
 }
--- ReactPart3(ReactBasics)/CleanedUpReactProject/01-starting-setup/src/components/ExpenseItem.js
+++ ReactPart3(ReactBasics)/CleanedUpReactProject/01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
-const ExpenseItem = ()=>(
-    <h2>Expense Items!</h2>
-)
+import './ExpenseItem.css' 
 
+const ExpenseItem = () => {
+	// you can only hav one root element being returned
+	return (
+		<div className="expense-item">
+			<div>March 28th 2021</div>
+			<div className='expense-item__description'>
+				<h2>Car insurance</h2>
+				<div className='expense-item__price'>$292.34</div>
+			</div>
+		</div>
+	)
+}
 export default ExpenseItem
