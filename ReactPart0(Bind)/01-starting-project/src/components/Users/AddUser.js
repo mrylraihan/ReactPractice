ea6bad5326663c21f731367e992bdf8ad61cd735
mrylraihan <mr.ylraihan@gmail.com>
2022-01-23 17:23:32 -0500

    working with apis and bind

--- ReactPart0(Bind)/01-starting-project/src/components/Users/AddUser.js
+++ ReactPart0(Bind)/01-starting-project/src/components/Users/AddUser.js
@@ -6,10 +6,11 @@ import axios from 'axios'
 import { BsFillAlarmFill } from 'react-icons/bs'
 const AddUser = props => {
    
-        const fetchData = async () => {
+    const website = 'https://api.kanye.rest'
+        const fetchData = async (web) => {
             try {
                 // const res = await getKanyeQuote()//method that does api call
-                const res = await axios.get('https://api.kanye.rest') //or we could have dont it this way 
+                const res = await axios.get(web) //or we could have dont it this way 
                 console.log(res);
                 console.log(res.data);
                 props.onAddQuote(res.data.quote)
@@ -22,7 +23,8 @@ const AddUser = props => {
     return (
         <Card className={styles.input}>
             <h1>Lets get a Quotes! <BsFillAlarmFill/></h1>
-            <Button onClick={() => fetchData()}>Get a Quote!</Button>
+            {/* <Button onClick={() => fetchData()}>Get a Quote!</Button> */}
+            <Button onClick={fetchData.bind(this,website)}>Get a Quote!</Button>
         </Card>
     )
 }
