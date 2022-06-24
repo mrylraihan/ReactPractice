ee3e5864f55fd3ae33ca203daacbda7af297d1da
mrylraihan <mr.ylraihan@gmail.com>
2022-06-23 19:13:27 -0700

    just playing around

---
+++ ReactPart0(Login&Crud)/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import LoginPage from './components/Pages/LoginPage';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactPart0(Login&Crud)/src/components/Pages/LoginPage.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useRef } from 'react'
+import {Button, Form} from 'react-bootstrap'
+const LoginPage = () => {
+    const [user, setUser] = useState({})
+    const email = useRef()
+    const password = useRef()
+    const submitHandler = (e)=>{
+        e.preventDefault()
+        setUser(
+            {
+                email: email.current.value,
+                password: password.current.value
+            }
+        )
+    }
+    console.log(user);
+    return (
+        <div>
+            <Form onSubmit={submitHandler}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" ref={email} />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" ref={password}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    )
+}
+
+export default LoginPage
