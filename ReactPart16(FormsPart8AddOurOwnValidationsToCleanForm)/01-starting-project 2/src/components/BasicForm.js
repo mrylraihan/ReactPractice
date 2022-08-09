f2a37d58f12a753040b9880eff86bcf6279a21d9
mrylraihan <mr.ylraihan@gmail.com>
2022-08-08 17:18:45 -0700

    juss incase

--- ReactPart16(FormsPart8AddOurOwnValidationsToCleanForm)/01-starting-project 2/src/components/BasicForm.js
+++ ReactPart16(FormsPart8AddOurOwnValidationsToCleanForm)/01-starting-project 2/src/components/BasicForm.js
@@ -37,20 +37,20 @@ if(fNIsValid && lNIsValid && emailIsValid){
         <div className='form-control'>
           <label htmlFor='name'>First Name</label>
           <input type='text' id='name' value={fName} onBlur={fNTouchedHandler} onChange={fNameChangeHandler}/>
-          {fNHasError && <p>you didnt write anything!</p>}
+          {fNHasError && <p className='error-text'>you didnt write anything!</p>}
         </div>
         {/* lastName */}
         <div className={lastNClasses}>
           <label htmlFor='name'>Last Name</label>
           <input type='text' id='name' value={lName} onChange={lNameChangeHandler} onBlur={lNTouchedHandler}/>
-          {lNHasError && <p>you didnt write anything!</p>}
+          {lNHasError && <p className='error-text'>you didnt write anything!</p>}
         </div>
       </div>
       {/* email */}
       <div className={emailClasses}>
         <label htmlFor='name'>E-Mail Address</label>
         <input type='text' id='name' value={email} onChange={emailChangeHandler} onBlur={emailTouchedHandler}/>
-        {emailHasError && <p>Note an email</p>}
+        {emailHasError && <p className='error-text'>Note an email</p>}
       </div>
       <div className='form-actions'>
         <button disabled={!formIsValid}>Submit</button>
